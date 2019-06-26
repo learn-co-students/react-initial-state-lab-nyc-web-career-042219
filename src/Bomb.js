c94987f0bf1160585645c6a427b12b5814b3a91d
Tim Koar <koartimothy@gmail.com>
2019-06-26 14:11:45 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
-// your Bomb code here!
+import React, {Component} from 'react'
+
+export default class Bomb extends Component {
+
+  state = {
+    secondsLeft: this.props.initialCount
+  }
+
+  subtractSeconds = (event) => {
+    setInterval(() => {
+      this.setState({secondsLeft: this.state.secondsLeft - 1} )
+    }, 1000);
+  }
+
+  render() {
+    return(
+      <div>
+      <p onClick={this.subtractSeconds}> {this.state.secondsLeft}, seconds before I go boom!</p>
+       </div>
+    )
+  }
+
+}
